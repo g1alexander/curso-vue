0489c00a2bc3addf4a7c5bfadf992d7ea8b842ef
g1alexander <alexlds26@gmail.com>
2022-02-05 11:04:09 -0500

    init app2

---
+++ app2/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import PokemonPage from "./pages/PokemonPage.vue";
+</script>
+
+<template>
+  <img
+    alt="Vue logo"
+    class="fade-in"
+    src="./assets/logo.svg"
+    width="125"
+    height="125"
+  />
+
+  <PokemonPage />
+</template>
+
+<style>
+@import "./css/base.css";
+@import "./css/animation.css";
+</style>
---
+++ app2/src/pages/PokemonPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>¿Quien es el pokemon?</h1>
+
+  //TODO: img pokemon //TODO: opciones pokemon
+</template>
+
+<style></style>
