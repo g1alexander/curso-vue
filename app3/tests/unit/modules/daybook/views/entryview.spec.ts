dd34c5845e291a71bd42880da457f8bb9451c7d4
g1alexander <alexlds26@gmail.com>
2022-07-20 15:29:36 -0500

    seccion 13 terminada tests

--- app3/tests/unit/modules/daybook/views/entryview.spec.ts
+++ app3/tests/unit/modules/daybook/views/entryview.spec.ts
@@ -5,6 +5,7 @@ import { createStore } from "vuex";
 import { journalModule } from "@/modules/daybook/store";
 import { JournalState } from "@/modules/daybook/store/interface/State";
 import { journalState } from "../../../mocks/journal-state";
+import Swal from "sweetalert2";
 
 const createVuexStore = (initialState: JournalState) =>
   createStore({
@@ -16,12 +17,22 @@ const createVuexStore = (initialState: JournalState) =>
     },
   });
 
+jest.mock("sweetalert2", () => ({
+  fire: jest.fn(),
+  showLoading: jest.fn(),
+  close: jest.fn(),
+}));
+
+const mockSwal = Swal as jest.Mocked<typeof Swal>;
+
 describe("tests componet EntryView", () => {
   const $router = {
     push: jest.fn(),
   };
 
   const store = createVuexStore(journalState);
+
+  store.dispatch = jest.fn();
 
   let wrapper = shallowMount(EntryViewVue, {
     props: { id: "-MwZ3AI4JD9U9p5PJ3ay" },
@@ -49,5 +60,39 @@ describe("tests componet EntryView", () => {
   test("debe de mostrar la entrada correctamente", () => {
     expect(wrapper.html()).toMatchSnapshot();
     expect(wrapper.vm.$router.push).not.toHaveBeenCalled();
+  });
+
+  test("debe de borrar la entrada y salir", (done) => {
+    mockSwal.fire.mockReturnValueOnce(
+      Promise.resolve({
+        isConfirmed: true,
+        isDenied: false,
+        isDismissed: false,
+      })
+    );
+
+    const btn = wrapper.find(".btn-danger");
+
+    btn.trigger("click");
+
+    expect(mockSwal.fire).toHaveBeenCalledWith({
+      showDenyButton: true,
+      title: "¿Estás seguro?",
+      text: "Esta acción no se puede deshacer",
+      icon: "warning",
+      confirmButtonText: "Sí, borrar",
+    });
+
+    setTimeout(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(
+        "journalModule/deleteEntry",
+        "-MwZ3AI4JD9U9p5PJ3ay"
+      );
+      expect(wrapper.vm.$router.push).toHaveBeenCalledWith({
+        name: "no-entry",
+      });
+
+      done();
+    }, 1);
   });
 });
